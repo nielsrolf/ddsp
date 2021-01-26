5c9bcc60a9977c1f4cdad8f2d2e88dfe986a2486
nielsrolf <niels.warncke@gmail.com>
2021-01-26 18:58:08 +0100

    naive timbre interpolation

--- ddsp/colab/jupyter_utils.py
+++ ddsp/colab/jupyter_utils.py
@@ -1,10 +1,15 @@
 import ddsp
+import ddsp.training
 from matplotlib import pyplot as plt
 import numpy as np
 from IPython.display import display, Audio
 
 
 DEFAULT_SAMPLE_RATE = ddsp.spectral_ops.CREPE_SAMPLE_RATE
+
+specplot = ddsp.training.plotting.specplot
+plot_impulse_responses = ddsp.training.plotting.plot_impulse_responses
+transfer_function = ddsp.training.plotting.transfer_function
 
 
 def show_audio(wav, sample_rate=DEFAULT_SAMPLE_RATE, focus_points=[0.3], focus_windows=[1000]):
@@ -16,6 +21,7 @@ def show_audio(wav, sample_rate=DEFAULT_SAMPLE_RATE, focus_points=[0.3], focus_w
     assert isinstance(wav, np.ndarray)
   wav = wav.squeeze()
   display(Audio(data=wav, rate=sample_rate))
+  specplot(wav)
   
   _, axes = plt.subplots(nrows=1+len(focus_points), ncols=1, figsize=(15, 2+2*len(focus_points)), sharey=True)
   
