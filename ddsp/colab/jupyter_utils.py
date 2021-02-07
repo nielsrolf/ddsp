db44a9542e34220a9fec2ecde0cb9b9300321fed
nielsrolf <niels.warncke@gmail.com>
2021-02-07 18:07:36 +0100

    some stuff moved to jupyter utils

--- ddsp/colab/jupyter_utils.py
+++ ddsp/colab/jupyter_utils.py
@@ -46,4 +46,52 @@ def dict_shape(d, prefix=""):
     for k, v in d.items():
       print(f"{prefix}{k}")
       dict_shape(v, prefix+"  ")
-      
+
+
+def get_dataset_statistics(data_provider, batch_size=1):
+  """Calculate dataset stats and save in a pickle file."""
+  print('Calculating dataset statistics for', data_provider)
+  data_iter = iter(data_provider.get_batch(batch_size, repeats=1))
+
+  # Unpack dataset.
+  i = 0
+  loudness = []
+  f0 = []
+  f0_conf = []
+  audio = []
+
+  for batch in data_iter:
+    loudness.append(batch['loudness_db'])
+    f0.append(batch['f0_hz'])
+    f0_conf.append(batch['f0_confidence'])
+    audio.append(batch['audio'])
+    i += 1
+
+  print(f'Computing statistics for {i * batch_size} examples.')
+
+  loudness = np.vstack(loudness)
+  f0 = np.vstack(f0)
+  f0_conf = np.vstack(f0_conf)
+  audio = np.vstack(audio)
+
+  # Fit the transform.
+  trim_end = 20
+  f0_trimmed = f0[:, :-trim_end]
+  l_trimmed = loudness[:, :-trim_end]
+  f0_conf_trimmed = f0_conf[:, :-trim_end]
+  mask_on, _ = detect_notes(l_trimmed, f0_conf_trimmed)
+  quantile_transform = fit_quantile_transform(l_trimmed, mask_on)
+
+  # Average values.
+  mean_pitch = np.mean(ddsp.core.hz_to_midi(f0_trimmed[mask_on]))
+  mean_loudness = np.mean(l_trimmed)
+  mean_max_loudness = np.mean(np.max(l_trimmed, axis=0))
+
+  # Object to pickle all the statistics together.
+  ds = {'mean_pitch': mean_pitch,
+        'mean_loudness': mean_loudness,
+        'mean_max_loudness': mean_max_loudness,
+        'quantile_transform': quantile_transform}
+  
+  return ds
+
